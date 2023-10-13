8f618781a0f09f7f54adbf289c101f9ea95ba945
Ababil Mustax <95958484+birdfromhell@users.noreply.github.com>
2023-10-13 13:35:40 +0700

    Refactor main.py and remove redundant method.py
    This commit refactors the main.py file to make code more modular and readability high. The changes include creation of functions for each specific task such as send_request, write_file and check_status_code (previously this was all lumped together) to improve code structure. The logic part of the original main.py is moved into a main function to make it easier to test. Furthermore, it modernizes the user inputs using the 'rich' library for better UI.
    
    On the other hand, the method.py file is removed because it contained identical POST request methods as main.py and was causing redundancy in our codebase.

--- main.py
+++ main.py
@@ -1,60 +1,73 @@
 import requests
-
-api = input("Masukan Api anda (https://www.remove.bg/api) 0 Untuk API Default? ")
-nama = input("Masukan Nama File (Tanpa extension file : jpg ,png? ")
-metod = int(input("Masukan File Lewat 1.Link - 2.Folder ? "))
-lewat = False
-color = ""
-link = None
-path = None
-
-if api == "0":
-    api = str("BW5D7u8MGCqfcKtxCMBnUopc")
-
-if metod == 1:
-    link = input("Masukan Link ?")
-    bgcolor = input("Masukan Warna Background (Ex:red,green / hex:81d4fa ? Enter --> Tidak ? ")
-    lewat = True
-
-else:
-    path = input("Masukan path File ?")
-    bgcolor = input("Masukan Warna Background (Ex:red,green / hex:81d4fa ? Enter --> Tidak ? ")
-    lewat = False
-
-if bgcolor == "":
-    color = bgcolor
-else:
-    color = bgcolor
-
-if lewat:
-    response = requests.post(
-        'https://api.remove.bg/v1.0/removebg',
-        data={
-            'image_url': link,
-            'size': 'auto',
-            'bg_color': color
-        },
-        headers={'X-Api-Key': api},
-    )
-else:
-    response = requests.post(
-        'https://api.remove.bg/v1.0/removebg',
-        files={'image_file': open(path, 'rb')},
-        data={'size': 'auto'},
-        headers={'X-Api-Key': api},
-    )
-
-if response.status_code == requests.codes.ok:
-    with open(f'{nama}.png', 'wb') as out:
-        out.write(response.content)
+from rich import print
+from rich.prompt import Prompt
 
 
-match response.status_code:
-    case 403:
-        print("Api Key Salah")
-    case 429:
-        print("Rate limit exceeded")
-    case 200:
-        print("Finish")
+def prompt_user() -> dict:
+    api = str("BW5D7u8MGCqfcKtxCMBnUopc") if Prompt.ask("Masukan Api anda (https://www.remove.bg/api) 0 Untuk API Default? ") == "0" else ""
+    file_name = Prompt.ask("Masukan Nama File (Tanpa extension file : jpg ,png)")
+    method = Prompt.ask("Masukan File Lewat 1.Link - 2.Folder ", choices=["1", "2"], default="1")
+    bgcolor = Prompt.ask("Masukan Warna Background (Ex:red,green / hex:81d4fa ? Enter --> Tidak")
+    source = Prompt.ask("Masukan Link") if method == "1" else Prompt.ask("Masukan path File")
 
-#8e82c6
+    return {
+        'api': api,
+        'file_name': file_name,
+        'method': method,
+        'bgcolor': bgcolor,
+        'source': source
+    }
+
+
+def send_request(data: dict):
+    response = ''
+    headers = {'X-Api_Key': data['api']}
+
+    if data['method'] == '1':
+        response = requests.post(
+            'https://api.remove.bg/v1.0/removebg',
+            data={
+                'image_url': data['source'],
+                'size': 'auto',
+                'bg_color': data['bgcolor']
+            },
+            headers=headers
+        )
+    else:
+        with open(data['source'], 'rb') as image_file:
+            response = requests.post(
+                'https://api.remove.bg/v1.0/removebg',
+                files={'image_file': image_file},
+                data={'size': 'auto'},
+                headers=headers,
+            )
+
+    return response
+
+
+def write_file(response, file_name):
+    if response.status_code == requests.codes.ok:
+        with open(f'{file_name}.png', 'wb') as out:
+            out.write(response.content)
+
+
+def check_status_code(response):
+    match response.status_code:
+        case 403:
+            print("[bold red]Api Key Salah[/]")
+        case 429:
+            print("[bold red]Rate limit exceeded[/]")
+        case 200:
+            print("[bold green]Finish[/]")
+
+
+def main():
+    user_inputs = prompt_user()
+    response = send_request(user_inputs)
+    check_status_code(response)
+    if response.status_code == requests.codes.ok:
+        write_file(response, user_inputs['file_name'])
+
+
+if __name__ == "__main__":
+    main()
